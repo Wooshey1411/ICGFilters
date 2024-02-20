23f3fbb80b5fac40171cc4795e3fba1bfdceda7c
Wooshey1411 <vad2003.barnaul@gmail.com>
2024-02-20 17:17:19 +0700

    initial commit

---
+++ src/main/java/ru/nsu/icg/lab2/Main.java
@@ -0,0 +1,9 @@
+package ru.nsu.icg.lab2;
+
+import ru.nsu.icg.lab2.gui.MainWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
---
+++ src/main/java/ru/nsu/icg/lab2/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package ru.nsu.icg.lab2.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        super("FilterShop");
+        setMinimumSize(new Dimension(640,480));
+        setPreferredSize(new Dimension(800,800));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+}
