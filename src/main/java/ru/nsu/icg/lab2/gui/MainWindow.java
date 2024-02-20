f63632fbfc864a9923c5143cfedb4408f13c9377
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-02-20 20:40:20 +0700

    Попытка связывания

--- src/main/java/ru/nsu/icg/lab2/gui/MainWindow.java
+++ src/main/java/ru/nsu/icg/lab2/gui/MainWindow.java
@@ -7,11 +7,17 @@ public class MainWindow extends JFrame {
 
     public MainWindow(){
         super("FilterShop");
+
+        final JLabel label = new JLabel("Связал, проверяй");
+        label.setFont(new Font("Monospaced", Font.BOLD, 82));
+        add(label);
+
         setMinimumSize(new Dimension(640,480));
         setPreferredSize(new Dimension(800,800));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         setVisible(true);
+
     }
     private void setMenu(){
 
