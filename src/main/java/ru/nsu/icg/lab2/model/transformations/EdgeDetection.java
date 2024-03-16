ffa256420c0b0a2650f4b313d6a8c3f4f01e2665
pipichinka <m.sirotkin@g.nsu.ru>
2024-03-16 12:20:36 +0700

    added EdgeDetection

--- src/main/java/ru/nsu/icg/lab2/model/transformations/EdgeDetection.java
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/EdgeDetection.java
@@ -5,9 +5,19 @@ import ru.nsu.icg.lab2.model.ImageFactory;
 import ru.nsu.icg.lab2.model.ImageInterface;
 import ru.nsu.icg.lab2.model.Transformation;
 
+import java.util.Arrays;
+
 @Data
 public class EdgeDetection implements Transformation {
-    private int filterSize;
+    public enum FilterType{
+        ROBERTS, SOBEL
+    }
+
+
+    private FilterType type = FilterType.SOBEL;
+
+
+    private int brightnessBorder = 128;
 
     private final ImageFactory imageFactory;
 
@@ -15,8 +25,141 @@ public class EdgeDetection implements Transformation {
         this.imageFactory = imageFactory;
     }
 
+
+    private ImageInterface applyRoberts(ImageInterface oldImage){
+        int borderStep = 1;
+        int width = oldImage.getWidth();
+        int height = oldImage.getHeight();
+        int gridSize = height * width;
+        int[] grid = new int[gridSize];
+        oldImage.getARGB(grid);
+        int[] newGrid = new int[gridSize];
+        Arrays.fill(newGrid, 0xFF000000);
+
+        //without borders
+        for (int y = 0; y < height - borderStep; y++){
+            for (int x = 0; x < width - borderStep; x++){
+                int index = y * width + x;
+                int colorTopLeft = grid[index];
+                int colorTopRight = grid[index + 1];
+                int colorBottomLeft = grid[index + width];
+                int colorBottomRight = grid[index + width + 1];
+                int red = 0;
+                int green = 0;
+                int blue = 0;
+
+                int colorXRed = ((colorBottomRight & 0x00FF0000) >> 16) - ((colorTopLeft & 0x00FF0000) >> 16);
+                int colorYRed = ((colorBottomLeft & 0x00FF0000) >> 16) - ((colorTopRight & 0x00FF0000) >> 16);
+                int colorXGreen = ((colorBottomRight & 0x0000FF00) >> 8) - ((colorTopLeft & 0x0000FF00) >> 8);
+                int colorYGreen = ((colorBottomLeft & 0x0000FF00) >> 8) - ((colorTopRight & 0x0000FF00) >> 8);
+                int colorXBlue = ((colorBottomRight & 0x000000FF)) - ((colorTopLeft & 0x000000FF));
+                int colorYBlue = ((colorBottomLeft & 0x000000FF)) - ((colorTopRight & 0x000000FF));
+                int deltaRed = (int) Math.sqrt(colorXRed * colorXRed + colorYRed * colorYRed);
+                int deltaGreen = (int) Math.sqrt(colorXGreen * colorXGreen + colorYGreen * colorYGreen);
+                int deltaBlue = (int) Math.sqrt(colorXBlue * colorXBlue + colorYBlue * colorYBlue);
+
+                if (deltaRed > brightnessBorder || deltaGreen > brightnessBorder || deltaBlue > brightnessBorder){
+                    red = 255;
+                    green = 255;
+                    blue = 255;
+                }
+
+                newGrid[index] |= (red << 16) | (green << 8) | blue;
+            }
+        }
+
+        ImageInterface imageInterface = imageFactory.createImage(oldImage.getWidth(),oldImage.getHeight());
+        imageInterface.setARGB(newGrid);
+        return imageInterface;
+    }
+
+
+    private ImageInterface applySobel(ImageInterface oldImage){
+        int borderStep = 1;
+        int width = oldImage.getWidth();
+        int height = oldImage.getHeight();
+        int gridSize = height * width;
+        int[] grid = new int[gridSize];
+        oldImage.getARGB(grid);
+        int[] newGrid = new int[gridSize];
+        Arrays.fill(newGrid, 0xFF000000);
+
+        int[][] matrixX = new int[3][3];
+        matrixX[0][0] = -1;
+        matrixX[0][1] = -2;
+        matrixX[0][2] = -1;
+        matrixX[1][0] = 0;
+        matrixX[1][1] = 0;
+        matrixX[1][2] = 0;
+        matrixX[2][0] = 1;
+        matrixX[2][1] = 2;
+        matrixX[2][2] = 1;
+
+        int[][] matrixY = new int[3][3];
+        matrixY[0][0] = -1;
+        matrixY[0][1] = 0;
+        matrixY[0][2] = 1;
+        matrixY[1][0] = -2;
+        matrixY[1][1] = 0;
+        matrixY[1][2] = 2;
+        matrixY[2][0] = -1;
+        matrixY[2][1] = 0;
+        matrixY[2][2] = 1;
+
+        //without borders
+        for (int y = borderStep; y < height - borderStep; y++){
+            for (int x = borderStep; x < width - borderStep; x++){
+                int index = y * width + x;
+                int redX = 0;
+                int greenX = 0;
+                int blueX = 0;
+                int redY = 0;
+                int greenY = 0;
+                int blueY = 0;
+                for (int inX = - borderStep; inX <= borderStep; inX++){
+                    for (int inY = -borderStep; inY <= borderStep; inY++){
+                        int inXIndex = inX + borderStep;
+                        int inYIndex = inY + borderStep;
+                        int gridIndex = index + inY * width + inX;
+                        int pixelColor = grid[gridIndex];
+                        redX +=  ((((pixelColor & 0x00FF0000) >> 16) * matrixX[inYIndex][inXIndex]));
+                        greenX +=  ((((pixelColor & 0x0000FF00) >> 8) * matrixX[inYIndex][inXIndex]));
+                        blueX +=  ((((pixelColor & 0x000000FF)) * matrixX[inYIndex][inXIndex]));
+                        redY +=  ((((pixelColor & 0x00FF0000) >> 16) * matrixY[inYIndex][inXIndex]));
+                        greenY +=  ((((pixelColor & 0x0000FF00) >> 8) * matrixY[inYIndex][inXIndex]));
+                        blueY +=  ((((pixelColor & 0x000000FF)) * matrixY[inYIndex][inXIndex]));
+                    }
+                }
+                int deltaRed = (int) Math.sqrt(redX * redX + redY * redY);
+                int deltaBlue = (int) Math.sqrt(blueX * blueX + blueY * blueY);
+                int deltaGreen = (int) Math.sqrt(greenX * greenX + greenY * greenY);
+                int red = 0;
+                int green = 0;
+                int blue = 0;
+
+                if (deltaRed > brightnessBorder || deltaGreen > brightnessBorder || deltaBlue > brightnessBorder){
+                    red = 255;
+                    green = 255;
+                    blue = 255;
+                }
+
+                newGrid[index] |= (red << 16) | (green << 8) | blue;
+            }
+        }
+
+
+
+        ImageInterface imageInterface = imageFactory.createImage(oldImage.getWidth(),oldImage.getHeight());
+        imageInterface.setARGB(newGrid);
+        return imageInterface;
+    }
+
+
     @Override
     public ImageInterface apply(ImageInterface oldImage) {
-        return null;
+        if (type == FilterType.SOBEL){
+            return applySobel(oldImage);
+        }
+        return applyRoberts(oldImage);
     }
 }
