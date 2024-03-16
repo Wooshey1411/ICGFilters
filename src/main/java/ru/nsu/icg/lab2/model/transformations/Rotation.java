746d6b29b58841f7b61e5a55ac97615fc792c0cd
pipichinka <m.sirotkin@g.nsu.ru>
2024-03-17 01:02:15 +0700

    Rotation 2.0

--- src/main/java/ru/nsu/icg/lab2/model/transformations/Rotation.java
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/Rotation.java
@@ -20,52 +20,52 @@ public class Rotation implements Transformation {
 
     @Override
     public ImageInterface apply(ImageInterface oldImage) {
-        System.out.println( "rotation " + degrees);
         int width = oldImage.getWidth();
         int height = oldImage.getHeight();
         int centerX = width / 2;
         int centerY = height / 2;
         double sin = Math.sin(((double) degrees / 180.0) * Math.PI);
         double cos = Math.cos(((double) degrees / 180.0) * Math.PI);
-        System.out.println("sin " + sin + " cos " + cos);
+
         int topLeftX = (int) ((-centerX) * cos) - (int) (centerY * sin);
         int topLeftY = (int) ((-centerX) * sin) + (int) (centerY * cos);
-        System.out.println("top left (" + topLeftX + ", " + topLeftY + ")");
+
         int topRightX = (int) (centerX * cos) - (int) (centerY * sin);
         int topRightY = (int) (centerX * sin) + (int) (centerY * cos);
-        System.out.println("top right (" + topRightX + ", " + topRightY + ")");
+
         int botLeftX = (int) ((-centerX) * cos) + (int) (centerY * sin);
         int botLeftY = (int) ((-centerX) * sin) - (int) (centerY * cos);
-        System.out.println("bot left (" + botLeftX + ", " + botLeftY + ")");
+
         int botRightX = (int) (centerX * cos) + (int) (centerY * sin);
         int botRightY = (int) (centerX * sin) - (int) (centerY * cos);
-        System.out.println("bot right (" + botRightX + ", " + botRightY + ")");
 
         int newHeight = Math.max(Math.abs(topLeftY - botRightY), Math.abs(topRightY - botLeftY)) + 1;
         int newWidth = Math.max(Math.abs(botRightX - topLeftX), Math.abs(topRightX - botLeftX)) + 1;
-        System.out.println("new height " + newHeight);
-        System.out.println("new width " + newWidth);
-        int gridSize = newHeight * newWidth;
-        int[] grid = new int[height * width];
+        int newGridSize = newHeight * newWidth;
+        int gridSize = height * width;
+        int[] grid = new int[gridSize];
         oldImage.getARGB(grid);
-        int[] newGrid = new int[gridSize];
+        int[] newGrid = new int[newGridSize];
         Arrays.fill(newGrid, 0xFFFFFFFF);
 
         int newCenterX = newWidth / 2;
         int newCenterY = newHeight / 2;
 
-
-        //without borders
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width ; x++){
-                int index = y * width + x;
-                int xR = x - centerX;
-                int yR = -y + centerY;
-                int newX = (int) (xR * cos) - (int) (yR * sin);
-                int newY = (int) (xR * sin) + (int) (yR * cos);
-                System.out.println("x " + xR +" y " + yR + " newX " + newX + " newY " + newY);
-                int newIndex = newWidth * (-(newY - newCenterY)) + (newX + newCenterX);
-                newGrid[newIndex] = grid[index];
+        for (int y = 0; y < newHeight; y++){
+            for (int x = 0; x < newWidth ; x++){
+                int index = y * newWidth + x;
+                int xR = x - newCenterX;
+                int yR = -y + newCenterY;
+                int newX = (int) (xR * cos) + (int) (yR * sin);
+                int newY = (int) -(xR * sin) + (int) (yR * cos);
+                int oldY = (-(newY - centerY));
+                int oldX = (newX + centerX);
+                if (oldX < 0 || oldX >= width || oldY < 0 || oldY >= height){
+                    newGrid[index] |= 0x00FFFFFF;
+                    continue;
+                }
+                int oldIndex = width * oldY + oldX;
+                newGrid[index] = grid[oldIndex];
             }
         }
 
