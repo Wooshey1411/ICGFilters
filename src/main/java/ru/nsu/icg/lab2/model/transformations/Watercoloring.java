5479572eb2e03d2102bee987438c38acb2a57402
pipichinka <m.sirotkin@g.nsu.ru>
2024-03-16 18:31:57 +0700

    add Watercoloring

--- src/main/java/ru/nsu/icg/lab2/model/transformations/Watercoloring.java
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/Watercoloring.java
@@ -4,15 +4,60 @@ import ru.nsu.icg.lab2.model.ImageFactory;
 import ru.nsu.icg.lab2.model.ImageInterface;
 import ru.nsu.icg.lab2.model.Transformation;
 
+import java.util.Arrays;
+
 public class Watercoloring implements Transformation {
 
     private final ImageFactory imageFactory;
+
+    private int windowSize = 5;
 
     public Watercoloring(ImageFactory imageFactory) {
         this.imageFactory = imageFactory;
     }
     @Override
     public ImageInterface apply(ImageInterface oldImage) {
-        return null;
+        int borderStep = (windowSize - 1) / 2;
+        int width = oldImage.getWidth();
+        int height = oldImage.getHeight();
+        int gridSize = height * width;
+        int[] grid = new int[gridSize];
+        oldImage.getARGB(grid);
+        int[] newGrid = new int[gridSize];
+        Arrays.fill(newGrid, 0xFF000000);
+        int middle = (windowSize * windowSize + 1) / 2 ;
+        int[] red = new int[windowSize * windowSize];
+        int[] green = new int[windowSize * windowSize];
+        int[] blue = new int[windowSize * windowSize];
+
+
+        //without borders
+        for (int y = borderStep; y < height - borderStep; y++){
+            for (int x = borderStep; x < width - borderStep; x++){
+                int index = y * width + x;
+                int i = 0;
+                for (int inX = - borderStep; inX <= borderStep; inX++){
+                    for (int inY = -borderStep; inY <= borderStep; inY++){
+                        int gridIndex = index + inY * width + inX;
+                        int pixelColor = grid[gridIndex];
+                        red[i] = (pixelColor & 0x00FF0000) >> 16;
+                        green[i] = (pixelColor & 0x0000FF00) >> 8;
+                        blue[i] = pixelColor & 0x000000FF;
+                        i++;
+                    }
+                }
+                Arrays.sort(red);
+                Arrays.sort(green);
+                Arrays.sort(blue);
+                newGrid[index] |= (red[middle] << 16) | (green[middle] << 8) | blue[middle];
+            }
+        }
+
+
+        ImageInterface imageInterface = imageFactory.createImage(oldImage.getWidth(),oldImage.getHeight());
+        imageInterface.setARGB(newGrid);
+
+        Sharpening sharpening = new Sharpening(imageFactory);
+        return sharpening.apply(imageInterface);
     }
 }
