c45f2d9d2cf2235d99d369563cd6c865939cc49b
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-03-19 01:16:00 +0700

    Выделил медианный фильтр и отрефакторил его и акварелизацию

---
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/MedianFilter.java
@@ -0,0 +1,104 @@
+package ru.nsu.icg.lab2.model.transformations;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.nsu.icg.lab2.model.ImageFactory;
+import ru.nsu.icg.lab2.model.ImageInterface;
+import ru.nsu.icg.lab2.model.Transformation;
+
+import java.util.Arrays;
+
+@Setter
+@Getter
+public class MedianFilter extends Transformation {
+    private int windowSize = 5;
+
+    public MedianFilter(ImageFactory imageFactory) {
+        super(imageFactory);
+    }
+
+    @Override
+    public ImageInterface apply(ImageInterface oldImage) {
+        final int windowVolume = windowSize * windowSize;
+        final int radius = (windowSize - 1) / 2;
+        final int middle = (windowVolume + 1) / 2 ;
+        final int width = oldImage.getWidth();
+        final int height = oldImage.getHeight();
+        final int[] oldGrid = oldImage.getARGB();
+        final int[] newGrid = new int[width * height];
+        final int[] red = new int[windowVolume];
+        final int[] green = new int[windowVolume];
+        final int[] blue = new int[windowVolume];
+
+        // Process center of image
+        for (int y = radius; y < height - radius; y++){
+            for (int x = radius; x < width - radius; x++){
+                final int index = y * width + x;
+                final int oldAlpha = TransformationUtils.getAlpha(oldGrid[index]);
+
+                getSortedNeighbours(
+                        radius,
+                        index,
+                        width,
+                        oldGrid,
+                        red,
+                        green,
+                        blue
+                );
+
+                newGrid[index] = TransformationUtils.getARGB(
+                        oldAlpha,
+                        red[middle],
+                        green[middle],
+                        blue[middle]
+                );
+            }
+        }
+
+        // Process borders
+        for (int y = 0; y < radius; y++) {
+            System.arraycopy(oldGrid, y * width, newGrid, y * width, width);
+        }
+        for (int y = height - radius; y < height; y++) {
+            System.arraycopy(oldGrid, y * width, newGrid, y * width, width);
+        }
+        for (int x = 0; x < radius; x++) {
+            for (int y = radius; y < height - radius; y++) {
+                newGrid[y * width + x] = oldGrid[y * width + x];
+            }
+        }
+        for (int x = width - radius; x < width; x++) {
+            for (int y = radius; y < height - radius; y++) {
+                newGrid[y * width + x] = oldGrid[y * width + x];
+            }
+        }
+
+        return getImageFactory().createImage(oldImage, newGrid);
+    }
+
+    private static void getSortedNeighbours(
+            int radius,
+            int offset,
+            int width,
+            int[] grid,
+            int[] red,
+            int[] green,
+            int[] blue
+    ) {
+        int componentIndex = 0;
+        for (int inX = - radius; inX <= radius; inX++){
+            for (int inY = -radius; inY <= radius; inY++){
+                final int gridIndex = offset + inY * width + inX;
+                final int pixelColor = grid[gridIndex];
+                red[componentIndex] = TransformationUtils.getRed(pixelColor);
+                green[componentIndex] = TransformationUtils.getGreen(pixelColor);
+                blue[componentIndex] = TransformationUtils.getBlue(pixelColor);
+                componentIndex++;
+            }
+        }
+
+        Arrays.sort(red);
+        Arrays.sort(green);
+        Arrays.sort(blue);
+    }
+}
--- src/main/java/ru/nsu/icg/lab2/model/transformations/Watercoloring.java
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/Watercoloring.java
@@ -1,62 +1,29 @@
 package ru.nsu.icg.lab2.model.transformations;
 
-import lombok.Getter;
-import lombok.Setter;
 import ru.nsu.icg.lab2.model.ImageFactory;
 import ru.nsu.icg.lab2.model.ImageInterface;
 import ru.nsu.icg.lab2.model.Transformation;
 
-import java.util.Arrays;
-
 public class Watercoloring extends Transformation {
+    private final MedianFilter medianFilter;
     private final Sharpening sharpening;
 
-    @Getter
-    @Setter
-    private int windowSize = 5;
+    public int getWindowSize() {
+        return medianFilter.getWindowSize();
+    }
+
+    public void setWindowSize(int windowSize) {
+        medianFilter.setWindowSize(windowSize);
+    }
 
     public Watercoloring(ImageFactory imageFactory) {
         super(imageFactory);
+        medianFilter = new MedianFilter(imageFactory);
         sharpening = new Sharpening(imageFactory);
     }
 
     @Override
     public ImageInterface apply(ImageInterface oldImage) {
-        int borderStep = (windowSize - 1) / 2;
-        int width = oldImage.getWidth();
-        int height = oldImage.getHeight();
-        int gridSize = height * width;
-        int[] grid = new int[gridSize];
-        oldImage.getARGB(grid);
-        int[] newGrid = new int[gridSize];
-        Arrays.fill(newGrid, 0xFF000000);
-        int middle = (windowSize * windowSize + 1) / 2 ;
-        int[] red = new int[windowSize * windowSize];
-        int[] green = new int[windowSize * windowSize];
-        int[] blue = new int[windowSize * windowSize];
-
-        //without borders
-        for (int y = borderStep; y < height - borderStep; y++){
-            for (int x = borderStep; x < width - borderStep; x++){
-                int index = y * width + x;
-                int i = 0;
-                for (int inX = - borderStep; inX <= borderStep; inX++){
-                    for (int inY = -borderStep; inY <= borderStep; inY++){
-                        int gridIndex = index + inY * width + inX;
-                        int pixelColor = grid[gridIndex];
-                        red[i] = (pixelColor & 0x00FF0000) >> 16;
-                        green[i] = (pixelColor & 0x0000FF00) >> 8;
-                        blue[i] = pixelColor & 0x000000FF;
-                        i++;
-                    }
-                }
-                Arrays.sort(red);
-                Arrays.sort(green);
-                Arrays.sort(blue);
-                newGrid[index] |= (red[middle] << 16) | (green[middle] << 8) | blue[middle];
-            }
-        }
-
-        return sharpening.apply(getImageFactory().createImage(oldImage, newGrid));
+        return sharpening.apply(medianFilter.apply(oldImage));
     }
 }
