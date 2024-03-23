1776b5a8a7c82904d7e2dcdef15a22cf27f0897d
pipichinka <m.sirotkin@g.nsu.ru>
2024-03-23 11:56:13 +0700

    Победил поворот!!!

--- src/main/java/ru/nsu/icg/lab2/model/transformations/Rotation.java
+++ src/main/java/ru/nsu/icg/lab2/model/transformations/Rotation.java
@@ -22,44 +22,43 @@ public class Rotation extends Transformation {
     public ImageInterface apply(ImageInterface oldImage) {
         int width = oldImage.getWidth();
         int height = oldImage.getHeight();
-        int centerX = width / 2;
-        int centerY = height / 2;
-        double sin = Math.sin(((double) degrees / 180.0) * Math.PI);
-        double cos = Math.cos(((double) degrees / 180.0) * Math.PI);
+        double centerX = (width - 1) / 2.0;
+        double centerY = (height - 1) / 2.0;
+        double sin = Math.sin((degrees / 180.0) * Math.PI);
+        double cos = Math.cos((degrees / 180.0) * Math.PI);
+        if (Math.abs(degrees) == 180){
+            sin = 0.0;
+        }
+        double topLeftX =  (((-centerX) * cos) -  (centerY * sin));
+        double topLeftY =  (((-centerX) * sin) + (centerY * cos));
 
-        int topLeftX = (int) ((-centerX) * cos) - (int) (centerY * sin);
-        int topLeftY = (int) ((-centerX) * sin) + (int) (centerY * cos);
+        double  topRightX =  ((centerX * cos) - (centerY * sin));
+        double  topRightY =  ((centerX * sin) + (centerY * cos));
 
-        int topRightX = (int) (centerX * cos) - (int) (centerY * sin);
-        int topRightY = (int) (centerX * sin) + (int) (centerY * cos);
+        double  botLeftX =  (((-centerX) * cos) + (centerY * sin));
+        double  botLeftY =  (((-centerX) * sin) - (centerY * cos));
 
-        int botLeftX = (int) ((-centerX) * cos) + (int) (centerY * sin);
-        int botLeftY = (int) ((-centerX) * sin) - (int) (centerY * cos);
+        double  botRightX =  ((centerX * cos) + (centerY * sin));
+        double  botRightY =  ((centerX * sin) - (centerY * cos));
 
-        int botRightX = (int) (centerX * cos) + (int) (centerY * sin);
-        int botRightY = (int) (centerX * sin) - (int) (centerY * cos);
-
-        int newHeight = Math.max(Math.abs(topLeftY - botRightY), Math.abs(topRightY - botLeftY)) + 1;
-        int newWidth = Math.max(Math.abs(botRightX - topLeftX), Math.abs(topRightX - botLeftX)) + 1;
+        int newHeight = (int) Math.max(Math.abs(topLeftY - botRightY), Math.abs(topRightY - botLeftY)) + 1;
+        int newWidth = (int) Math.max(Math.abs(botRightX - topLeftX), Math.abs(topRightX - botLeftX)) + 1;
         int newGridSize = newHeight * newWidth;
         final int[] oldGrid = oldImage.getGrid();
         final int[] newGrid = new int[newGridSize];
         Arrays.fill(newGrid, 0xFFFFFFFF);
-
-        int newCenterX = newWidth / 2;
-        int newCenterY = newHeight / 2;
-
+        double newCenterX = (newWidth - 1) / 2.0;
+        double newCenterY = (newHeight - 1) / 2.0;
         for (int y = 0; y < newHeight; y++){
             for (int x = 0; x < newWidth ; x++){
                 int index = y * newWidth + x;
-                int xR = x - newCenterX;
-                int yR = -y + newCenterY;
-                int newX = (int) (xR * cos) + (int) (yR * sin);
-                int newY = (int) -(xR * sin) + (int) (yR * cos);
-                int oldY = (-(newY - centerY));
-                int oldX = (newX + centerX);
+                double xR = x - newCenterX;
+                double yR = -y + newCenterY;
+                double newX = ((xR * cos) +  (yR * sin));
+                double newY = (-(xR * sin) + (yR * cos));
+                int oldY = (int) Math.floor(-(newY - centerY));
+                int oldX = (int) Math.floor (newX + centerX);
                 if (oldX < 0 || oldX >= width || oldY < 0 || oldY >= height){
-                    newGrid[index] |= 0x00FFFFFF;
                     continue;
                 }
                 int oldIndex = width * oldY + oldX;
